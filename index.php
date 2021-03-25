17569208b6c4a43b8b9384aa15d01570d764a7d9
OguzBayat <oguzbayat77@gmail.com>
2021-03-26 00:47:27 +0300

    update title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>deneme</title>
+	<title>Hacker New Clone</title>
 	<link rel="stylesheet" href="style.css" type="text/css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
